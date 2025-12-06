95c24ae8a02c020862f4a70aca615ba7abd86baf
Alex Idowu <alexidowu25@gmail.com>
2025-12-07 00:47:30 +0100

    Fix rexec command not found for non-root users in Zsh
    Modified create_zshrc function in shell_setup.go to explicitly include /root/.local/bin and /home/user/.local/bin in the PATH. This resolves the 'rexec: command not found' issue for non-root users who were not correctly inheriting the PATH to the rexec CLI, especially in Ubuntu containers.

--- internal/container/shell_setup.go
+++ internal/container/shell_setup.go
@@ -315,7 +315,7 @@ export ZSH="$HOME/.oh-my-zsh"
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export TERM=xterm-256color
-export PATH="$HOME/.local/bin:$PATH"
+export PATH="/root/.local/bin:/home/user/.local/bin:$PATH"
 
 ZSH_THEME="%s"
 
